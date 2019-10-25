bfc66bf71674682d124a881611f43f987795b09a
cishocksr <cishockleysr@gmail.com>
2019-10-24 21:15:50 -0400

    working search

--- nba/src/App.js
+++ nba/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import './App.css';
-import AccountCreation from './components/AccountCreation';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login';
-import Homepage from './components/Homepage'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import AccountCreation from "./components/AccountCreation";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-        <h2 className='header'>
-          Welcome to NBA Career Longevity Projector
-        </h2>
-          <Route exact path='/' component={Homepage} />
-          <Route exact path='/account' component = {AccountCreation} />
+          <h2 className="header">Welcome to NBA Career Longevity Projector</h2>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/account" component={AccountCreation} />
           <Route exact path="/login" component={Login} />
         </header>
       </div>
