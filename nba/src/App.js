29d80360574a70d987a1ebb13cb64ffe161096f9
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-10-24 20:30:46 -0600

    add dummy data

--- nba/src/App.js
+++ nba/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import AccountCreation from './components/AccountCreation';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
-import Homepage from './components/Homepage'
+import Homepage from './components/Homepage';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path='/' component={Homepage} />
           <Route exact path='/account' component = {AccountCreation} />
           <Route exact path="/login" component={Login} />
+          <Route exact path='/dashboard' component={Dashboard}/>
         </header>
       </div>
     </Router>
